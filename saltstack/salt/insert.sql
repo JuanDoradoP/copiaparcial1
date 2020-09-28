d01d9c9d6444e7c326dbd25eeb4524471174fb47
German2004 <gcarvajal2404@gmail.com>
2020-09-28 13:25:27 -0500

    Delete .sql file

--- saltstack/salt/insert.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO cellphones (id, name, brand, capacity) VALUES (1, 'note8', 'xiaomi', 128);
-INSERT INTO cellphones (id, name, brand, capacity) VALUES (2, 'p20', 'huawei', 128);
-INSERT INTO cellphones (id, name, brand, capacity) VALUES (3, 'a71', 'samsung', 128);
